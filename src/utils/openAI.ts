f2bb7233b6e697ea759cb69ad60ac51990f14c62
7wheeler <7wheeler@gmail.com>
2026-03-16 19:28:14 -0500

    Update openAI.ts

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -33,7 +33,7 @@ export const generatePayload = (apiKey: string, messages: ChatMessage[]): Reques
       model,
       messages: formattedMessages,
       temperature: 0.6,
-      max_tokens: maxTokens,
+      max_completion_token: maxTokens,
       stream: true,
     }),
   }
